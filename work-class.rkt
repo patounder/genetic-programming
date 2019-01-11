d17e0b9c886da1eb068a5d3ef70b4cd7a5f2c8af
Patricio Quiroz <pmquirozvargas@gmail.com>
2019-01-11 19:03:07 -0300

    finish tree basic methods

--- work-class.rkt
+++ work-class.rkt
@@ -48,6 +48,25 @@ return the best-so-far individual (may be is necessary transform AST to concrete
                 (void))
               )))
 
+;copy-ast :: ast -> ast
+;return copy from ast
+(define (copy-ast ast)
+  ast)
+
+(test (copy-ast (add (num 5) (num 6)))
+      (add (num 5) (num 6)))
+
+;print-ast :: ast -> void
+;print-ast in the terminal
+(define (print-ast ast)
+  (print ast))
+
+(test (print-ast (num 5))
+      (void))
+
+
+(define (interp-tree tree)
+  (interp tree))
 ;grow :: int -> ast
 ;return ast from set available (with terminal and non-terminal nodes). Implement grow algorithm
 (define (grow depth)
@@ -157,30 +176,24 @@ return the best-so-far individual (may be is necessary transform AST to concrete
 ;make-child :: member member -> member
 ;return new member instance, apply crossover (child) between mother and father instances, then mutation in new child
 (define (make-child mother father)
-  (let (#;[mother-like-list (get-nil-ast-height (get-height mother))])
-    (begin
-      (def new-child (crossover mother father))
-      (mutation new-child)
-      )))
-
+  (let ([new-child (crossover mother father)])
+    (mutation new-child)
+    ))
 
 ;crossover :: member member -> member
 ;return a new member instance with copy and mix genes from params (mother and father)
-(define (crossover mother father)      
+(define (crossover mother father)
   (letrec ([child-list (set-ast-values (send 'get-ast mother) (get-nil-ast-height (get-height (send 'get-ast mother))) 0)]
            [crossover-point (random (length child-list))]
            ;TODO create father's subtree defined with root from crossover-point in the offspring, must be contain a height defined with height from crossover point in the child's subtree
-           ;TODO remove because list must already exist in the object
-           [father-like-list (set-ast-values (send 'get-ast father) (get-nil-ast-height (get-height (send 'get-ast father))) 0)]
-           [father-random-subtree (get-sub-tree father-like-list (random (length father-like-list)))]
+           [father-random-subtree (get-sub-tree (send 'get-ast-list father) (random (length (send 'get-ast-list father))))]
            [clean-child-list (clean-subtree child-list crossover-point)]
            )
     (set-value-list clean-child-list crossover-point father-random-subtree 2)))
 
 (define mother-member (new-instance class-member 0 '(+ 1 2) (add (num 1) (num 2)) 3))
 (define father-member (new-instance class-member 0 '(* + - 1 5 9 7) (mult (add (num 1) (num 5)) (sub (num 9) (num 7))) 10))
-(crossover mother-member father-member)
-      
+;(crossover mother-member father-member)
 
 ;mutation :: member -> member
 ;return a new instance member with crossover random subtree
